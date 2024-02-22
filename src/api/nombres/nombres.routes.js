051db813a7e2d51535c24e4647d0ed7fc312d3e8
Miguel <miguelaf71@hotmail.com>
2024-02-23 00:14:44 +0100

    eliminada ruta post nombre

--- src/api/nombres/nombres.routes.js
+++ src/api/nombres/nombres.routes.js
@@ -2,7 +2,7 @@ const nombresRoutes = require("express").Router();
 
 const { postNombre, getNombres, deleteNombre, editNombre } = require("./nombres.controller");
 
-nombresRoutes.post("/", postNombre);
+// nombresRoutes.post("/", postNombre);
 nombresRoutes.delete("/:idNombre", deleteNombre)
 nombresRoutes.get("/", getNombres);
 nombresRoutes.put("/:idNombre", editNombre)
