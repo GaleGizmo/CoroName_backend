cbd11b15679c7e9a2a0cf16a0aa14e8ebb3c4591
Miguel <miguelaf71@hotmail.com>
2024-03-07 23:54:27 +0100

    postnombre activo

--- src/api/nombres/nombres.routes.js
+++ src/api/nombres/nombres.routes.js
@@ -2,9 +2,9 @@ const nombresRoutes = require("express").Router();
 
 const { postNombre, getNombres, deleteNombre, editNombre, addVoto } = require("./nombres.controller");
 
-// nombresRoutes.post("/", postNombre);
+nombresRoutes.post("/", postNombre);
 nombresRoutes.delete("/:idNombre", deleteNombre)
 nombresRoutes.get("/", getNombres);
 nombresRoutes.put("/:idNombre", editNombre)
-nombresRoutes.post("/addVoto/:idCorista", addVoto)
+// nombresRoutes.post("/addVoto/:idCorista", addVoto)
 module.exports = nombresRoutes;
